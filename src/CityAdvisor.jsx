a043a288ae24b69c53610010c1a0cf83567e4eb6
BENHAMZA Sofiane <71550283+sofiane-benhamza@users.noreply.github.com>
2024-05-23 13:54:15 +0100

    Update CityAdvisor.jsx

--- src/CityAdvisor.jsx
+++ src/CityAdvisor.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import backgroundImage from "./img/bg.gif";
 
 export default function CityAdvisor() {
-  const [connected, setConnection] = useState(false);
+  const [connected, setConnection] = useState(true);
 
   //in case of refresh, check if user already logged in
   // Reading the value from localStorage
